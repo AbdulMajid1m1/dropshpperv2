932bb4971655408b3235c320a3b89d7524ddafe7
unknown <abdulmajid1m2@gmail.com>
2022-11-03 03:03:20 -0700

    driver regiter route

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,7 @@ module.exports = function isAuth(req, res, next) {
   if (req.session.isAuth) {
     next();
   } else {
-    res.redirect("/login");
+    // res.redirect("/login");
+    res.status(404).json({err:"no data"})
   }
 };
