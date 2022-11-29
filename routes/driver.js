7e66f3be9732fabe570e995c810ad0f6940607f2
unknown <abdulmajid1m2@gmail.com>
2022-11-29 14:22:43 +0500

    update driver addess

--- routes/driver.js
+++ routes/driver.js
@@ -8,14 +8,184 @@ const isAuth = require("../middleware/auth");
 
 // --------------------- NEW API  START-------------------////
 
+//////// --------------- POST REQUEST START -------------//////////////
 
+router.post("/update/driver-profile/:id", function (req, res) {
+  // let uniqueId =
+  //   req.user == undefined ? req.app.locals.userId._id : req.user._id;
 
+  Driver.findOne({ _id: req.params.id }, function (err, user) {
+    if (!err) {
+      try {
+        // const userPicture = req.files.userPicture;
+        // cloudinary.uploader.upload(userPicture.tempFilePath, (err, picture) => {
+
+        if (user.username === req.body.username) {
+          Driver.findOneAndUpdate(
+            { _id: req.params.id },
+            // {
+            //   $set: {
+            //     fullName:
+            //       req.body.fullName.length === 0
+            //         ? user.fullName
+            //         : req.body.fullName,
+            //     mobileNumber:
+            //       req.body.mobileNumber.length === 0
+            //         ? user.mobileNumber
+            //         : req.body.mobileNumber,
+            //     username:
+            //       req.body.username.length === 0
+            //         ? user.username
+            //         : req.body.username,
+            //     userPicture: picture.url,
+            //   },
+            // },
+            {
+              $set: {
+                fullName:
+                  req.body.fullName.length === 0
+                    ? user.fullName
+                    : req.body.fullName,
+                mobileNumber:
+                  req.body.mobileNumber.length === 0
+                    ? user.mobileNumber
+                    : req.body.mobileNumber,
+                username:
+                  req.body.username.length === 0
+                    ? user.username
+                    : req.body.username,
+              },
+            },
+            { new: true },
+            (err, result) => {
+              if (err) {
+                res.status(400).json({ Error: err });
+              } else {
+                return res.status(200).json({ DriverData: result });
+              }
+            }
+          );
+        } else {
+          Driver.findOne({ username: req.body.username }, (err, foundUser) => {
+            if (err) {
+              res.status(400).json({ Error: err });
+            } else {
+              if (foundUser === null) {
+                Driver.findOneAndUpdate(
+                  { _id: req.params.id },
+                  // {
+                  //   $set: {
+                  //     fullName:
+                  //       req.body.fullName.length === 0
+                  //         ? user.fullName
+                  //         : req.body.fullName,
+                  //     mobileNumber:
+                  //       req.body.mobileNumber.length === 0
+                  //         ? user.mobileNumber
+                  //         : req.body.mobileNumber,
+                  //     username:
+                  //       req.body.username.length === 0
+                  //         ? user.username
+                  //         : req.body.username,
+                  //     userPicture: picture.url,
+                  //   },
+                  // },
+                  {
+                    $set: {
+                      fullName:
+                        req.body.fullName.length === 0
+                          ? user.fullName
+                          : req.body.fullName,
+                      mobileNumber:
+                        req.body.mobileNumber.length === 0
+                          ? user.mobileNumber
+                          : req.body.mobileNumber,
+                      username:
+                        req.body.username.length === 0
+                          ? user.username
+                          : req.body.username,
+                    },
+                  },
+                  { new: true },
+                  (err, result) => {
+                    if (err) {
+                      res.status(400).json({ Error: err });
+                    } else {
+                      return res.status(200).json({ UpdatedInfo: result });
+                    }
+                  }
+                );
+              } else {
+                res.send("This username already in use");
+              }
+            }
+          });
+        }
+      } catch (e) {
+        res.status(400).json({ Error: e });
+      }
+    }
+    if (err) {
+      res.status(400).json({ Error: err });
+    }
+  });
+});
+
+// Address update form
+
+router.post("/update/driver-address/:id", function (req, res) {
+  Driver.findOne({ _id: req.params.id }, (err, user) => {
+    if (err) {
+      return res.json(err);
+    } else {
+      Driver.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $set: {
+            country:
+              req.body.country.length === 0 ? user.country : req.body.country,
+            region:
+              req.body.region.length === 0 ? user.region : req.body.region,
+            city: req.body.city.length === 0 ? user.city : req.body.city,
+            streetAddress:
+              req.body.streetAddress.length === 0
+                ? user.streetAddress
+                : req.body.streetAddress,
+          },
+        },
+        { new: true },
+        (err, result) => {
+          if (err) {
+            res.status(400).json({ Error: err });
+          } else {
+            res.status(200).json({ UpdatedData: result });
+          }
+        }
+      );
+    }
+  });
+});
+//////// --------------- POST REQUEST END -------------//////////////
 
 // --------------------- NEW API END-------------------////
 //Selecting Drivers Category.
 router.get("/drivers", isAuth, function (req, res) {
   res.send("Drivers Registration Page");
 });
+
+
+/////  GET DIVER DATA START /////////////
+router.get("/get-driver-data/:id", function (req, res) {
+  Driver.findOne({ _id: req.params.id }, function (err, user) {
+    if (!err) {
+      res.status(200).json({ DriverData: user, uuid: user._id });
+    }
+    if (err) {
+      res.status(400).json({ Error: err });
+    }
+  });
+});
+/////  GET DIVER DATA END /////////////
 
 // Seach results from driver page
 router.get("/drivers/search/:searchText", isAuth, function (req, res) {
